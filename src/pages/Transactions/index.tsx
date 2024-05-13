bca4c32e41eab885925c1d8321a0fffadacc95c9
Fernando Futila <fernando.futila@gmail.com>
2024-05-13 19:03:39 -0300

    :sparkles: feat: add Transaction Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styles"
 
 
 
@@ -8,6 +9,32 @@ export function Transactions () {
     <div>
       <Header/>
       <Summary/>
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 17.400,00
+                </PriceHighlight></td>
+              <td>Venda</td>
+              <td>12/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Academia</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                -R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Atividade</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
