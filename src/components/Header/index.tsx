4ef4c0d628a81ec95d27718605b00422ee084df9
Fernando Futila <fernando.futila@gmail.com>
2024-05-13 23:54:27 -0300

    :sparkles: feat: add NewTransactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,28 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 import logoDtMoney from "../../assets/dtmoney-logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal";
+
 
 export function Header() {
   return (
   <HeaderContainer>
     <HeaderContent>
         <img src={logoDtMoney} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+
+        <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+
+           <NewTransactionModal/>
+
+        </Dialog.Root>
+       
     </HeaderContent>
   </HeaderContainer>)
 }
